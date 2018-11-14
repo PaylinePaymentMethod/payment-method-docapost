7f03b75e9a1e3d4b343237d7b72c70fae1236752
houssni.mlinde <houssni.mlinde@mythalesgroup.io>
2018-11-14 14:34:46 +0100

    correction test integration scenario initial OK

--- src/test/java/com/payline/payment/docapost/Integration/TestIt.java
+++ src/test/java/com/payline/payment/docapost/Integration/TestIt.java
@@ -1,21 +1,31 @@
 package com.payline.payment.docapost.Integration;
 
+import com.google.gson.Gson;
 import com.payline.payment.docapost.TestUtils;
+import com.payline.payment.docapost.bean.PaymentResponseSuccessAdditionalData;
 import com.payline.payment.docapost.service.impl.PaymentServiceImpl;
 import com.payline.payment.docapost.service.impl.PaymentWithRedirectionServiceImpl;
+import com.payline.payment.docapost.utils.config.ConfigEnvironment;
+import com.payline.payment.docapost.utils.config.ConfigProperties;
 import com.payline.pmapi.bean.payment.ContractProperty;
 import com.payline.pmapi.bean.payment.PaymentFormContext;
 import com.payline.pmapi.bean.payment.request.PaymentRequest;
+import com.payline.pmapi.bean.payment.response.PaymentResponse;
+import com.payline.pmapi.bean.payment.response.impl.PaymentResponseFailure;
 import com.payline.pmapi.bean.payment.response.impl.PaymentResponseFormUpdated;
+import com.payline.pmapi.bean.payment.response.impl.PaymentResponseSuccess;
 import com.payline.pmapi.integration.AbstractPaymentIntegration;
+import com.payline.pmapi.service.PaymentService;
+import com.payline.pmapi.service.PaymentWithRedirectionService;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
 
 import static com.payline.payment.docapost.TestUtils.*;
-import static com.payline.payment.docapost.utils.DocapostConstants.CONTRACT_CONFIG_CREDITOR_ID;
+import static com.payline.payment.docapost.utils.DocapostConstants.*;
 
 
 public class TestIt extends AbstractPaymentIntegration {
@@ -47,6 +57,7 @@ public class TestIt extends AbstractPaymentIntegration {
 
         //Saisie du numéro de téléphone
         String phoneNumber = setupNumber();
+        System.out.println("Wait 5-10 seconds... ");
 
         //step 2 Create a mandate
         //Saisir OTP
@@ -58,8 +69,7 @@ public class TestIt extends AbstractPaymentIntegration {
 
         //TODO REQUEST WITH PAYMENT RESPONSE STEP2
         Scanner keyboardOTP = new Scanner(System.in);
-
-
+        keyboardOTP.reset();
         System.out.println("Enter your  OTP : ");
         String otp = keyboardOTP.nextLine();
         System.out.print("You entered : ");
@@ -69,22 +79,25 @@ public class TestIt extends AbstractPaymentIntegration {
         // !!! PaymentSuccess renvoyé
         PaymentRequest paymentRequestStep3 = createCustomPaymentRequestStep3(requestContextMain, otp, phoneNumber);
 
-        testIt.fullRedirectionPayment(paymentRequestStep3, testIt.paymentService, testIt.paymentWithRedirectionService);
+        testIt.checkPaymentResponse(paymentRequestStep3, testIt.paymentService, testIt.paymentWithRedirectionService);
+
+        /*Below test method from parent Class
+        /Dont work now because payment method return a PaymentSuccessResponse vs PaymentRedirectReponse is expected
+        */
+//        testIt.fullRedirectionPayment(paymentRequestStep3, testIt.paymentService, testIt.paymentWithRedirectionService);
 
     }
+
 
     @Test
     public void fullPaymentTest() {
-////        request = createDefaultPaymentRequest();
-//        request = createDefaultPaymentRequestStep2(PHONE_NUMBER_TEST);
-//        this.fullRedirectionPayment(request, paymentService, paymentWithRedirectionService);
+
     }
 
     @Override
-    protected String payOnPartnerWebsite(String step) {
+    protected String payOnPartnerWebsite(String partnerUrl) {
         //we are not redirected to Docapost website
-        return null;
-//            return SUCCESS_URL;
+        return SUCCESS_URL;
     }
 
     @Override
@@ -100,4 +113,65 @@ public class TestIt extends AbstractPaymentIntegration {
     }
 
 
+    /*
+     * Test below inspired from parent Class method
+     * Check we have a good PaymentResponseSuccess
+     *
+     */
+    public void checkPaymentResponse(PaymentRequest paymentRequest, PaymentService paymentService, PaymentWithRedirectionService paymentWithRedirectionService) {
+        PaymentResponse paymentResponseFromPaymentRequest = paymentService.paymentRequest(paymentRequest);
+        this.checkPaymentResponseIsNotFailure(paymentResponseFromPaymentRequest);
+        this.checkPaymentResponseIsRightClass("paymentRequest", paymentResponseFromPaymentRequest, PaymentResponseSuccess.class);
+        PaymentResponseSuccess paymentResponseSuccess = (PaymentResponseSuccess) paymentResponseFromPaymentRequest;
+        Assertions.assertNotNull(paymentResponseSuccess.getTransactionDetails());
+        String partnerTransactionId = paymentResponseSuccess.getPartnerTransactionId();
+        Assertions.assertEquals(partnerTransactionId, paymentResponseSuccess.getPartnerTransactionId());
+        //Url to send ??
+        //Mock
+        String partnerUrl = paymentResponseSuccess.getStatusCode();
+        String redirectionUrl = this.payOnPartnerWebsite(partnerUrl);
+        Assertions.assertEquals("https://succesurl.com/", redirectionUrl);
+
+        //Check mandate
+        String mandateLink = this.getMandateUrl(paymentResponseSuccess);
+        Assertions.assertNotNull(mandateLink);
+        System.out.println("link to mandate below ");
+        System.out.println(mandateLink);
+
+    }
+
+    //Test inte KO PaymentresponseSuccess returned vs PaymentResponseRedirect
+    private void checkPaymentResponseIsNotFailure(PaymentResponse paymentResponse) {
+        Assertions.assertFalse(paymentResponse instanceof PaymentResponseFailure, () -> {
+            return "paymentRequest returned PaymentResponseFailure (Failure cause = " + ((PaymentResponseFailure) paymentResponse).getFailureCause() + ", errorCode = " + ((PaymentResponseFailure) paymentResponse).getErrorCode();
+        });
+    }
+
+    private void checkPaymentResponseIsRightClass(String requestName, PaymentResponse paymentResponse, Class clazz) {
+        Assertions.assertTrue(paymentResponse.getClass().isAssignableFrom(clazz), () -> {
+            return requestName + " did not return a " + clazz.getSimpleName() + " (" + paymentResponse.toString() + ")";
+        });
+    }
+
+
+    private String getMandateUrl(PaymentResponseSuccess paymentResponseStep3) {
+        String additionalDataJson = paymentResponseStep3.getTransactionAdditionalData();
+
+        Gson gson = new Gson();
+        PaymentResponseSuccessAdditionalData paymentResponseSuccessAdditionalData = gson.fromJson(additionalDataJson, PaymentResponseSuccessAdditionalData.class);
+
+        //Next step : download mandate
+        String mandateRum = paymentResponseSuccessAdditionalData.getMandateRum();
+
+
+        return ConfigProperties.get(CONFIG_SCHEME, ConfigEnvironment.DEV)
+                + "://"
+                + ConfigProperties.get(CONFIG_HOST, ConfigEnvironment.DEV)
+                + "/"
+                + ConfigProperties.get(CONFIG_PATH_WSMANDATE_MANDATE_PDFTPL)
+                + "/"
+                + GOOD_CREDITOR_ID
+                + "/"
+                + mandateRum;
+    }
 }
